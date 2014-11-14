36611330cb6c6a27840c91b07bc783b5125c2bf3
Łukasz Jendrzejek <jendrzejek_lukasz@wp.pl>
2014-11-14 18:00:03 +0100

    Pierwsze zmiany w komórkach, symulacja modeli jeszcze nie funkcjonuje.

--- statyka.m
+++ statyka.m
@@ -1,44 +1,57 @@
-% Skrypt służący do wstępnej symulacji obiektu układu zbiorników
-% rozpatrywanego w zadaniu 13. w projekcie z przedmiotu Sztuczna
-% Inteligencja w Automatyce.
+% Skrypt służący do wykreślania charakterystyk statycznych obiektu rozpatrywanego
+% w zadaniu 13. w projekcie z przedmiotu Sztuczna Inteligencja w Automatyce.
 
 global tanks;
 load( 'tanksParameters.mat' );
 
-dt = 20; % sekundy
-
+% Zmienne czasu wykorzystywane w symulacji obiektu. Wszystkie wartości podane
+% są w sekundach.
+dt = 20;
 time = [ 0 : dt : 3000 ];
 simulationLength = length(time);
 
+% Wartości sterowania dla których obiekt jest symulowany aż do uzyskania
+% stanu ustalonego.
 controlValues = [ 0.1 : 0.1 : 200 ];
-outputSSValues = zeros( length( controlValues ), 1 );
+controlOutputSSValues = zeros( length( controlValues ), 1 );
 
-inputTrajectory = tanks.sstate.F1 * ones( simulationLength, 1 );
-for i = dt * 20 : simulationLength
-   inputTrajectory(i) = inputTrajectory(i) + 0;
-end
+% Wartości zakłócenia dla których obiekt jest symulowany aż do uzyskania
+% stanu ustalonego.
+disturbanceValues = [ 0.1 : 0.1 : 40 ];
+disturbanceOutputSSValues = zeros( length( disturbanceValues ), 1 );
 
-disturbanceTrajectory = tanks.sstate.FD * ones( simulationLength, 1 );
-% for i = 1 : simulationLength
-%    disturbanceTrajectory(i) = disturbanceTrajectory(i) + 0;
-% end
+% Wektor do logowania wyjścia symulowanego obiektu.
+output = zeros( simulationLength, 1 );
 
-inputBuffer = tanks.sstate.F1 * ones( tanks.const.tau / dt, 1 );
-
-tanksState = [ 10102, tanks.const.C2*(tanks.sstate.h2)^2 ];
-
-plotInput = zeros( simulationLength, 1 );
-plotOutput = zeros( simulationLength, 1 );
-
+% Struktura opcji przekazywana dla procedury ode45. Dla rozważanego obiektu
+% wystarczające są wartości domyślne.
 opts = odeset();
 
-for j = 1 : length( controlValues )
+% Główna pętla algorytmu - dla wszystkich wartości wejścia i zakłócenia
+% po kolei symulowany jest obiekt. Symulacja trwa dostatecznie długo,
+% aby pewne było osiągnięcie stanu ustalonego obiektu.
+display('Pozyskiwanie charakterystyk statycznych obiektu:');
+for j = 1 : length(controlValues) + length(disturbanceValues)
 
-   inputTrajectory = controlValues(j) * ones( simulationLength, 1 );
-   inputBuffer = tanks.sstate.F1 * ones( tanks.const.tau / dt, 1 );
-   tanksState = [ 10102, tanks.const.C2*(tanks.sstate.h2)^2 ];
+   % Rozejście warunkowe do inicjalizacji wektorów wejściowych dla każdej
+   % symulacji odpowiednimi wartościami - stałym zakłóceniem, kiedy
+   % symulacja następuje dla kolejno zmienianych wartości sterowania
+   % i stałego sterowania dla podlegającego zmianie zakłócenia.
+   if j <= length(controlValues)
+      inputTrajectory = controlValues(j) * ones( simulationLength, 1 );
+      disturbanceTrajectory = tanks.sstate.FD * ones( simulationLength, 1 );
+      inputBuffer = tanks.sstate.F1 * ones( tanks.const.tau / dt, 1 );
+      tanksState = [ tanks.sstate.V1, tanks.sstate.V2 ];
+   else
+      inputTrajectory = tanks.sstate.F1 * ones( simulationLength, 1 );
+      disturbanceTrajectory = ...
+         disturbanceValues( j - length(controlValues) )*ones( simulationLength, 1 );
+      inputBuffer = tanks.sstate.F1 * ones( tanks.const.tau / dt, 1 );
+      tanksState = [ tanks.sstate.V1, tanks.sstate.V2 ];
+   end
 
-   display( controlValues(j) );
+   fprintf('Symulacja %d z %d\n', ...
+      j, length(controlValues) + length(disturbanceValues) );
 
    % ======== ZMIENNE WYŚWIETLAJĄCE POSTĘP SYMULACJI ================= %
    simulation_step = 0;                                                %
@@ -59,24 +72,35 @@ for j = 1 : length( controlValues )
          simulation_step = simulation_step + 1;                           %
       end                                                                 %
       % ================================================================= %
-   
+  
+      % Rozwiązywanie równań różniczkowych obiektu. 
       timeInterval = [ time(i), time(i) + dt ];
       u = inputBuffer( length(inputBuffer) );
       z = disturbanceTrajectory(i);
       inputBuffer = [ inputTrajectory(i); inputBuffer(1 : length(inputBuffer)-1 ) ];
    
-   
       [ tout, xout ] = ode45( @tanksFunction, timeInterval, tanksState, opts, u, z );
       tanksState = xout( size(xout, 1), : );
      
-      plotInput = inputTrajectory(i); 
-      plotOutput(i) = sqrt( tanksState(2) / tanks.const.C2 );
+      output(i) = sqrt( tanksState(2) / tanks.const.C2 );
       
    end
    
    fprintf('\n\n');
 
-   outputSSValues(j) = plotOutput( length(plotOutput) );
+   % Zapisywanie wartości wyjścia obiektu do odpowiednich wektorów (zmiana tylko
+   % sterowania i tylko zakłócenia).
+   if j <= length(controlValues)
+      controlOutputSSValues(j) = output( length(output) );
+   else
+      disturbanceOutputSSValues( j - length(controlValues) ) = ...
+         output( length(output) );
+   end
 end
 
 fprintf( 'Done!\n\n' );
+
+save( 'charakterystykiStatyczne.mat',...
+   'controlValues', 'controlOutputSSValues',...
+   'disturbanceValues', 'disturbanceOutputSSValues'...
+);
